fd25088b2d03d2ee13b305c0034164b5dfdc1d5e
Cihan Biyikli <git@cihan-biyikli.de>
2020-08-14 03:01:47 +0200

    desktop client dashboard v0.2

---
+++ smart-fabrics-dashboard/smart-fabrics-dashboard/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace smart_fabrics_dashboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnCOM = new System.Windows.Forms.Button();
+            this.comList = new System.Windows.Forms.ListBox();
+            this.txtBaudrate = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // btnCOM
+            // 
+            this.btnCOM.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(215)))));
+            this.btnCOM.Enabled = false;
+            this.btnCOM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCOM.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCOM.ForeColor = System.Drawing.Color.White;
+            this.btnCOM.Location = new System.Drawing.Point(93, 44);
+            this.btnCOM.Name = "btnCOM";
+            this.btnCOM.Size = new System.Drawing.Size(75, 32);
+            this.btnCOM.TabIndex = 0;
+            this.btnCOM.Text = "connect";
+            this.btnCOM.UseVisualStyleBackColor = false;
+            this.btnCOM.Click += new System.EventHandler(this.btnCOM_Click);
+            // 
+            // comList
+            // 
+            this.comList.BackColor = System.Drawing.SystemColors.Control;
+            this.comList.Font = new System.Drawing.Font("Cascadia Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comList.FormattingEnabled = true;
+            this.comList.ItemHeight = 15;
+            this.comList.Location = new System.Drawing.Point(12, 19);
+            this.comList.Name = "comList";
+            this.comList.Size = new System.Drawing.Size(75, 19);
+            this.comList.TabIndex = 2;
+            this.comList.SelectedIndexChanged += new System.EventHandler(this.comList_SelectedIndexChanged);
+            // 
+            // txtBaudrate
+            // 
+            this.txtBaudrate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBaudrate.Font = new System.Drawing.Font("Cascadia Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBaudrate.Location = new System.Drawing.Point(93, 18);
+            this.txtBaudrate.Name = "txtBaudrate";
+            this.txtBaudrate.Size = new System.Drawing.Size(75, 20);
+            this.txtBaudrate.TabIndex = 4;
+            this.txtBaudrate.Text = "115200";
+            this.txtBaudrate.TextChanged += new System.EventHandler(this.txtBaudrate_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(215)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "refresh";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Cascadia Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HideSelection = false;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(12, 89);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(156, 264);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cascadia Mono", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(12, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "refresh data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono SemiLight", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(174, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 32);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "| smart fabrics";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Mono SemiLight", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(227, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 28);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "dev dashboard";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(12, 359);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 21);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "sync baseline";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox2.Location = new System.Drawing.Point(12, 426);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(147, 21);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "show debug info";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(939, 450);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBaudrate);
+            this.Controls.Add(this.comList);
+            this.Controls.Add(this.btnCOM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnCOM;
+        private System.Windows.Forms.ListBox comList;
+        private System.Windows.Forms.TextBox txtBaudrate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ smart-fabrics-dashboard/smart-fabrics-dashboard/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Media;
+
+namespace smart_fabrics_dashboard
+{
+    public partial class Form1 : Form
+    {
+        Thread serialThread;
+        Boolean comport = false;
+        int[] mpr = new int[12];
+        int[] mprbase = new int[12];
+        PictureBox[,] box = new PictureBox[4, 4];
+        PictureBox[] bar = new PictureBox[12];
+        string[] ports;
+        Label[,] lbl = new Label[4, 4];
+        Boolean setBase = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ports = SerialPort.GetPortNames();
+            serialThread = new Thread(ReadSerial);
+            timer1.Interval = 100;
+
+            checkSerial();
+
+            listView1.Columns.Add("ID");
+            listView1.Columns[0].Width = 30;
+            listView1.Columns.Add("Base");
+            listView1.Columns.Add("Value");
+
+            for (int i = 0; i < 12; i++)
+            {
+                mpr[i] = 0;
+                mprbase[i] = 0;
+                listView1.Items.Add(i.ToString());
+                listView1.Items[i].SubItems.Add("-");
+                listView1.Items[i].SubItems.Add("-");
+            }
+            createGrid();
+        }
+
+        public void ReadSerial()
+        {
+            int id;
+            int val;
+            int baseline;
+            int count = 0;
+
+            while (comport)
+            {
+                string[] spa = serialPort1.ReadLine().Split(':');
+                id = Convert.ToInt32(spa[0]);
+                baseline = Convert.ToInt32(spa[1]);
+                val = Convert.ToInt32(spa[2]);
+                if (setBase)
+                {
+                    mprbase[id] = baseline;
+                    if (++count > 80)
+                    {
+                        count = 0;
+                        setBase = false;
+                    }
+                }
+                mpr[id] = val;
+            }
+        }
+
+        private void checkSerial()
+        {
+            foreach (string port in ports)
+            {
+                comList.Items.Add(port);
+            }
+
+            if (comList.Items.Count == 0)
+            {
+                comList.Items.Add("NO DEVICES FOUND");
+            }
+            else
+            {
+                comList.SetSelected(0, true);
+                btnCOM.Enabled = true;
+            }
+        }
+
+        private void btnCOM_Click(object sender, EventArgs e)
+        {
+            if (!comport)
+            {
+                comport = true;
+                serialPort1.Open();
+                serialThread.Start();
+                btnCOM.Text = "EXIT";
+                timer1.Enabled = true;
+
+            }
+            else
+            {
+                comport = false;
+                Application.Exit();
+            }
+        }
+
+        private void comList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comList.Items[0].ToString();
+        }
+
+        private void txtBaudrate_TextChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = txtBaudrate.Text;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            for (int i = 0; i < 12; i++)
+            {
+                listView1.BeginUpdate();
+                try
+                {
+                    if (setBase) listView1.Items[i].SubItems[1].Text = mprbase[i].ToString();
+                    listView1.Items[i].SubItems[2].Text = mpr[i].ToString();
+                }
+                finally
+                {
+                    listView1.EndUpdate();
+                }
+
+                bar[i].Width = mpr[i];
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int c = mpr[i] + mpr[j + 4];
+                    c = Convert.ToInt32(map(c, 0, mprbase[i] + mprbase[j + 4] + 20, 0, 255));
+                    box[i, j].BackColor = Color.FromArgb(c, c, c);
+                    lbl[i, j].Text = "(" + i + "," + j + ") " + c + " ["+ i+"/"+ (j+4) +"]";
+                }
+            }
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                listView1.Items[i].SubItems[1].Text = mprbase[i].ToString();
+            }
+        }
+
+
+        void createGrid()
+        {
+            Point pos = new Point(175, 120);
+            int padding = 8;
+            int size = 11;
+
+            for (int i = 0; i < 12; i++)
+            {
+                bar[i] = new PictureBox
+                {
+                    BackColor = Color.FromArgb(0, 173, 239),
+                    Height = size,
+                    Width = 100,
+                    Left = pos.X,
+                    Top = pos.Y + (i * (size + padding)),
+                };
+                this.Controls.Add(bar[i]);
+            }
+
+            pos = new Point(500, 15);
+            padding = 5;
+            size = 100;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    box[j, i] = new PictureBox
+                    {
+                        Width = size,
+                        Height = size,
+                        BackColor = Color.Gray,
+                        Left = (pos.X + (i * (size + padding))),
+                        Top = (pos.Y + (j * (size + padding))),
+                    };
+                    this.Controls.Add(box[j, i]);
+
+                    lbl[j, i] = new Label
+                    {
+                        AutoSize = true,
+                        Left = box[j, i].Left + 2,
+                        Top = box[j, i].Top + 2,
+                        ForeColor = Color.White,
+                        Font = new Font("Cascadia Mono", 7, FontStyle.Regular),
+                        Text = "",
+                        BackColor = Color.Black,
+                        Visible = false,
+                    };
+
+                    this.Controls.Add(lbl[j, i]);
+                    lbl[j, i].BringToFront();
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkSerial();
+        }
+
+        float map(float s, float a1, float a2, float b1, float b2)
+        {
+            return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            setBase = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (Label l in lbl)
+            {
+                l.Visible = checkBox2.Checked;
+            }
+        }
+    }
+}
